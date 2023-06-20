ae11752746f1634ddb5bf50cfd026674e327e144
9600bauds <blasaladio3@gmail.com>
2023-06-20 00:36:13 -0300

    Exercises 3.13.-3.14.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
@@ -21,33 +21,14 @@ app.use(
 const cors = require("cors");
 app.use(cors());
 
+const Person = require("./models/person");
+
 const generateId = () => {
   //Why are we using the arrow notation here?
   return Math.floor(Math.random() * 2147483647);
 };
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
+let persons = []; //I don't even need this??
 
 app.get("/", (request, response) => {
   response.send("How's this for self documenting?");
@@ -67,7 +48,13 @@ app.get("/info", (request, response) => {
 });
 
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({})
+    .then((persons) => {
+      response.json(persons);
+    })
+    .catch((error) => {
+      console.log("Could not fetch persons!", error.message);
+    });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -83,23 +70,29 @@ app.post("/api/persons", (request, response) => {
       error: "Person must have a phone number!",
     });
   }
-  for (const person of persons) {
+  /*for (const person of persons) {
     if (person.name === body.name) {
       return response.status(400).json({
         error: "Person already exists in the phonebook!",
       });
     }
-  }
+  }*/ //Forget about this for now.
 
-  const person = {
-    id: generateId(),
+  //ID is automatically generated
+  const person = new Person({
     name: body.name,
     number: body.number,
-  };
+  });
 
-  persons = persons.concat(person);
-
-  response.json(person);
+  //persons = persons.concat(person); //I don't understand why this isn't needed.
+  person
+    .save()
+    .then((savedPerson) => {
+      response.json(savedPerson);
+    })
+    .catch((error) => {
+      console.log("Could not save new person!", error.message);
+    });
 });
 
 app.get("/api/persons/:id", (request, response) => {
@@ -122,7 +115,6 @@ app.delete("/api/persons/:id", (request, response) => {
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
-
 app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
